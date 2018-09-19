64970df916f6a8e40f1d8b0ccc49080c20eb9a2e
whitfordr <whitfordR@berea.edu>
2018-09-19 19:35:57 -0400

    Made the thing

--- a03_stubs.py
+++ a03_stubs.py
@@ -1,43 +1,134 @@
-#################################################################################
-# Author:
-# Username:
-#
-# Assignment:
-# Purpose:
-#################################################################################
-# Acknowledgements:
-#
-#
-#################################################################################
+######################################################################
+# Author: Rebekah Whitford
+# Username: WhitfordR
+# Docs link: https://docs.google.com/document/d/1BSssHhYmqR5OmFmDy31BBGwogwgCtxlDFFBjO2v4img/edit?usp=sharing
 
-import turtle
+# Assignment: A03: A Pair of Fully Functional Gitty Psychedelic Robotic Turtles
+######################################################################
 
 
-def function_1():
+def make_house(shape):
     """
-    Docstring for function_1
+    Makes the rectangle that makes up the house
+
+    :return: None
     """
-    pass
-    # ...
+
+    shape.color("#4B0082")
+    shape.begin_fill()
+    for side in range(2):
+        shape.forward(140)
+        shape.right(90)
+        shape.forward(200)
+        shape.right(90)
+    shape.end_fill()
+
+def make_chimney(shape, x, y):
+    """
+    Adds a chimney to the house.
+
+    :param shape: the turtle
+    :param x: the x coordinate
+    :param y: the y coordinate
+    :return: None
+    """
+    shape.penup()
+    shape.setpos(x, y)
+    shape.pendown()
+    shape.color("#4B0082")
+    shape.begin_fill()
+    for side in range(2):
+        shape.forward(30)
+        shape.right(90)
+        shape.forward(20)
+        shape.right(90)
+    shape.end_fill()
+
+def make_window(shape, x, y):
+    """
+    Adds a window to the house.
+
+    :param shape: the turtle
+    :param x: the x coordinate
+    :param y: the y coordinate
+    :return: None
+    """
+    shape.penup()
+    shape.setpos(x, y)
+    shape.pendown()
+    shape.color('violet')
+    shape.begin_fill()
+    for side in range(2):
+        shape.forward(30)
+        shape.right(90)
+        shape.forward(20)
+        shape.right(90)
+    shape.end_fill()
+
+def make_door(shape):
+    """
+    Adds a door to the house.
+
+    :param shape: a Turtle object
+    :return: None
+    """
+    shape.penup()
+    shape.setpos(35, -150)
+    shape.pendown()
+    shape.color("violet")
+    shape.begin_fill()
+    for side in range(2):
+        shape.forward(85)
+        shape.right(90)
+        shape.forward(50)
+        shape.right(90)
+    shape.end_fill()
 
 
-def function_2():
+def make_smoke(shape):
     """
-    Docstring for function_2
+    adds smoke to the chimney
+    :return:
     """
-    pass
-    # ...
+    shape.penup()
+    shape.setpos(85, 30)
+    shape.color("gray")
+    shape.left(90)
+    shape.pendown()
+    shape.pensize(10)
+    for side in range(7):
+        shape.forward(15)
+        shape.right(90)
+        shape.forward(10)
+        shape.left(90)
 
 
 def main():
     """
-    Docstring for main
+    makes a house
+
+    :return: None
     """
-    # ...
-    function_1()            # Function call to function_1
-    function_2()            # Function call to function_2
+    import turtle               # allows use of the turtles library
+    shape = turtle.Turtle()
+    wn = turtle.Screen()        # Set up the window and its attributes
+    wn.bgcolor("lavender")
+    wn.title("lavender fields and farmhouse")
+
+    make_house(shape)
+
+    make_chimney(shape, 75, 20)
+
+    make_window(shape, 35, -80)
+    make_window(shape, 90, -80)
+    make_window(shape, 35, -30)
+    make_window(shape, 90, -30)
+
+    make_door(shape)
+
+    make_smoke(shape)
+
+    wn.exitonclick()
 
 
-main()
-
-
+main()                          # Calls the main function
