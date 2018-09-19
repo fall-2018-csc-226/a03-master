e18a005820a9127ba2d5875277722873a846e035
Brian Cox <coxb@berea.edu>
2018-09-19 10:37:39 -0400

    Added my code to the master, I created a house with a window. added my name and doc link to the read me file and my code file

--- a03_coxb.py
+++ a03_coxb.py
@@ -2,6 +2,8 @@
 # Author: Brian Cox & Ben Christson
 # Username: coxb & christsonb
 #
+#Google Doc: https://docs.google.com/document/d/1mzX6nEeJUVP-KF8fkC_UTUsFFCLvxnM_XIQvKY4xetM/edit?usp=sharing
+#
 # Assignment: A03: A Pair of Fully Functional Gitty Psychedelic Robotic Turtles
 # Purpose: To create a house
 ######################################################################
