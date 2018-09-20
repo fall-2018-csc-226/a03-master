2f2e7f1d4ef5bc6aea5fab0615cc529eeffcc84d
guthriet <guthriet@berea.edu>
2018-09-19 23:48:53 -0400

    Did all the thingos

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# a03-master
-A03: A Pair of Fully Functional Gitty Psychedelic Robotic Turtles
+# A03: A Pair of Psychedelic Gitty Functional Robotic Turtles
+
+Add your name to the list once you've completed this assignment:
+Thomas Guthrie
+https://docs.google.com/document/d/1Z26v8CJcrNvpc0HIH3Uz0_HQg0yQRM03k08LQRf7jVc/edit?usp=sharing
+
+- Scott Heggen:
--- a03_stubs.py
+++ a03_stubs.py
@@ -1,43 +1,111 @@
-#################################################################################
-# Author:
-# Username:
-#
-# Assignment:
-# Purpose:
-#################################################################################
-# Acknowledgements:
-#
-#
-#################################################################################
-
+###########################################################################################
+# sick nasty header block
+# Thomas Guthrie is did this
+# thank. Here code for shapes.
+###########################################################################################
 import turtle
+from time import sleep
 
 
-def function_1():
+#def func_setup():
+#    """
+#    this what do
+#    :return:
+#    """
+#    wn = turtle.Screen()
+#    wn.bgcolor(0, 0, 0)
+#    bob = turtle.Turtle()
+#    bob.color("white")
+#    bob.pensize(3)
+#    bob.penup()
+#    bob.setpos(-80, 0)
+#    return bob
+#    pass
+    # into the ass
+
+
+def func_doit(tName, w, h):
     """
-    Docstring for function_1
+    make house
+    tname, width, height
+    :return:
     """
+
+    tName.pendown()
+    tName.begin_fill()
+    for i in range(2):   # square builder 9000
+        tName.fd(w)
+        tName.left(90)
+        tName.fd(h)
+        tName.left(90)
+    tName.end_fill()
+    tName.fd(w*.5)    # tREEANGLE
+    tName.left(90)
+    tName.fd(h)
+    tName.right(90)
+    tName.begin_fill()
+    tName.fd(.75*w)
+    tName.left(135)
+    tName.fd(1.05*w)
+    tName.left(90)
+    tName.fd(1.05*w)
+    tName.left(135)
+    tName.fd(.75*w)
+    tName.end_fill()
+    tName.penup()
     pass
-    # ...
+    # into the ass
 
 
-def function_2():
+def func_doall(tName,a,rnd,flps):
     """
-    Docstring for function_2
+    this what do
+    tName,a,howmuchitspinny,flippies
+    :return:
     """
-    pass
-    # ...
+    tName.pendown()
+    for i in range(flps):
+        for i in range(rnd):
+            tName.backward(2)
+            tName.left(a)
+        if flps > 0:
+            for i in range(rnd):
+                tName.fd(2)
+                tName.left(a)
+            flps = flps - 1
+        else:
+            pass
+    tName.penup()
+    # into the ass
 
 
 def main():
     """
-    Docstring for main
+    this what do
+    :return:
     """
-    # ...
-    function_1()            # Function call to function_1
-    function_2()            # Function call to function_2
+    wn = turtle.Screen()
+    wn.bgcolor(0, 0, 0)
+    bob = turtle.Turtle()
+    bob.speed(0)
+    bob.color("white")
+    bob.pensize(3)
+    bob.penup()
+    bob.setpos(-80, 0)
+    func_doit(bob, 20, 10)
+    bob.setpos(10, 0)
+    func_doall(bob, 5, 77, 19,)
+    bob.setpos(100, 50)
+    func_doall(bob, 4, 75, 3)
+    bob.setpos(-80, -90)
+    func_doall(bob, 6, 20, 14)
+    bob.setpos(30, -70)
+    func_doall(bob, -32, 15, 24)
+    bob.setpos(150, 150)
+    sleep(5)
+    pass
+    # into the ass
 
 
 main()
-
 
