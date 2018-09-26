d1d920fc47f2b6233e210591d8599df2928a5e8a
hilljac <hilljac@berea.edu>
2018-09-26 19:18:59 -0400

    Fixed the README and header (google doc link is there now)

--- a03_stubs.py
+++ a03_stubs.py
@@ -4,7 +4,7 @@
 #
 # Assignment: A03: A Pair of Fully Functional Gitty Psychedelic Robotic Turtles
 # Purpose: Draws a cool picture
-#
+# google doc: https://docs.google.com/document/d/1gryHmJlAnhn5CQbHbTDOBaYJY4GSyt_H1st5oAbi1mM/edit#
 ######################################################################
 # Acknowledgements:
 
